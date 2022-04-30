eaf251aebe575d683a5d3d138922924ad99bb689
Sergey <>
2022-04-30 20:28:15 +0300

    представление сетки фильмов

--- moviesite/movie/models.py
+++ moviesite/movie/models.py
@@ -23,6 +23,8 @@ Title, logo, slug, content, data_ad, views, category, tags
 
 
 '''
+
+
 class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
@@ -38,6 +40,7 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
         ordering = ['title']
 
+
 # class Tag(models.Model):
 #     title = models.CharField(max_length=50)
 #     slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
@@ -48,22 +51,48 @@ class Category(models.Model):
 #     class Meta:
 #         ordering = ['title']
 
+
+class Quality(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('quality', kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = 'Качество'
+        verbose_name_plural = 'Качество'
+        ordering = ['title']
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
     logo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    
+
     content = models.CharField(max_length=5000, verbose_name='Описание')
     urlmov = models.CharField(max_length=500, verbose_name='Ссылка на видео')
     data_ad = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='movies', verbose_name='Категория')
     # tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    released = models.CharField(default='None', max_length=15, verbose_name='Год выхода')
+    director = models.CharField(default='None', max_length=50, verbose_name='Режиссер')
+    side = models.CharField(default='None', max_length=50, verbose_name='Страна', blank=True, null=True)
+    actors = models.CharField(default='None', max_length=5000, verbose_name='Актеры', blank=True, null=True)
+    genre = models.CharField(default='None', max_length=3000, verbose_name='Жанр', blank=True, null=True)
+    translation = models.CharField(default='None', max_length=3000, verbose_name='Перевод')
+    quality = models.ForeignKey(Quality, default='None', on_delete=models.PROTECT, related_name='movies',
+                                verbose_name='Качество')
+    rateKP = models.CharField(default='None', max_length=3000, verbose_name='Рейтинг КП')
+    rateIMDB = models.CharField(default='None', max_length=3000, verbose_name='Рейтинг IMDB')
 
     def __str__(self):
         return self.title
-        
-    
+
     def get_absolute_url(self):
         return reverse('post', kwargs={"slug": self.slug})
 
